8746ae8f8ef6278f488d6674ea9b0d8e03bc0d6a
Michael Newell <michael.newell212@gmail.com>
2023-06-22 16:12:53 -0600

    WIP

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express');
 const cors = require('cors')
+const User = require('./users/users');
 const server = express();
 
 const port = process.env.PORT || 9000
@@ -9,11 +10,44 @@ const port = process.env.PORT || 9000
 server.use(express.json());
 
 server.get('/api/users', (req, res) => {
-    res.json([])
+    User.find()
+    .then(users => {
+        if(!users) {
+            res.status(500).json({
+                message: "Unable to find users"
+            })
+        } else {
+            res.json(users)
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'error getting users',
+            err: err.message,
+            stack: err.stack 
+        })
+    })
 })
 
 server.post('/api/register', (req, res) => {
-
+    const user = req.body;
+    if(!user.username || !user.password) {
+        res.status(400).json({
+            message: "Name and password are required"
+        })
+    } else {
+        User.insert(user)
+        .then(createdUser => {
+            res.status(201).json(createdUser)
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: 'error creating user',
+                err: err.message,
+                stack: err.stack
+            })
+        })
+    }
 })
 
 server.use('*', (req, res, next) => {
@@ -31,3 +65,5 @@ server.use((err, req, res, next) => {
 server.listen(port, () => {
     console.log(`listening on ${port}`)
 })
+
+module.exports = server;
